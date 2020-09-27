9b974dbc056e968c89d03e043193250a0ff3aec3
Leonardo <leo_narita@hotmail.com>
2020-09-27 10:34:40 -0300

    Adicionando Javascrip para ocultar menus

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 // Nome Sobrenome (Prontuário)
 
+function hideList(list) {
+    var page = document.getElementById(list)
 
+    if(page.style.display !== 'none')
+        page.style.display = 'none';
+    else
+        page.style.display = 'block';
+}
